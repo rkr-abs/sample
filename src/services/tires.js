2c00ce1aa2593f32255c8dbe735980f74b40797e
RamKumar <rramkumar1306.com>
2022-12-01 21:23:15 +0530

    Refactored

--- src/services/tires.js
+++ src/services/tires.js
@@ -1,11 +1,7 @@
 import { map, range } from '@laufire/utils/collection';
 
 const size = 100;
-const left = 110;
-const top = 150;
-const two = 2;
 const three = 3;
-const fenSize = 120;
 const fenderDiff = 60;
 const fenderLeft = 30;
 const fenderTop = 90;
@@ -14,9 +10,6 @@ const hSize = 70;
 
 const tires = map(range(1, three), (number, i) => ({
 	size: size,
-	x: Math.pow(number, two) * left,
-	y: top,
-	fenderSize: fenSize * two,
 	fenderLeft: (i * fenderDiff) + fenderLeft,
 	fenderTop: fenderTop,
 	wSize: wSize,
